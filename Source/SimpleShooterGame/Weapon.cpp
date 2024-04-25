6d8b19e3a4b8003636d6c48943258e80578937d4
Cris Chong <81703600+tmseldon@users.noreply.github.com>
2024-04-24 23:58:59 -0400

    Fix animation speed and starting Weapon Class

---
+++ Source/SimpleShooterGame/Weapon.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapon.h"
+
+// Sets default values
+AWeapon::AWeapon()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot Weapon"));
+
+	if (Root != nullptr)
+	{
+		/*RootComponent = Root;*/
+		SetRootComponent(Root);
+	}
+
+	MeshWeapon = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Weapon Mesha Base"));
+	MeshWeapon->SetupAttachment(Root);
+}
+
+// Called when the game starts or when spawned
+void AWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AWeapon::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/SimpleShooterGame/Weapon.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Weapon.generated.h"
+
+UCLASS()
+class SIMPLESHOOTERGAME_API AWeapon : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AWeapon();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* Root;
+
+	UPROPERTY(VisibleAnywhere)
+	USkeletalMeshComponent* MeshWeapon;
+
+};
